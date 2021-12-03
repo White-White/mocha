297afcbe95e8d48482ed25df46cbd5a4cc100a68
white <white.zjh@antgroup.com>
2021-12-03 19:04:50 +0800

    Initial Commit

---
+++ Shared/mochaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mochaApp.swift
+//  Shared
+//
+//  Created by white on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct mochaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
